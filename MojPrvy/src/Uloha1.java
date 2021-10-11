f6f7648e6f99c465eb7e5331a24f69c141243dd8
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-10-11 10:48:55 +0200

    Create Uloha1.java

---
+++ MojPrvy/src/Uloha1.java
@@ -0,0 +1,13 @@
+
+public class Uloha1 {
+
+	public static void main(String[] args) {
+		System.out.println(" /\\_/\\      ----");
+		System.out.println("( ‘ ’ )   / Hello \\");
+		System.out.println("(  -  )  <  Junior |");
+		System.out.println(" | | |    \\ Coder!/");
+		System.out.println("(__|__)     ----");
+
+	}
+
+}
