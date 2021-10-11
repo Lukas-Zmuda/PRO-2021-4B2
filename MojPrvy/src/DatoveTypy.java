f5e69d5b883c61d9a10e26450ad4fbfbc7b4e1de
Lukas-Zmuda <l.zmuda@ostrovskeho.com>
2021-10-11 11:16:34 +0200

    datove typy + operatory

---
+++ MojPrvy/src/DatoveTypy.java
@@ -0,0 +1,36 @@
+
+public class DatoveTypy {
+
+	public static void main(String[] args) {
+		
+		//deklaracia premennej
+		int x;
+		
+		x = 123;
+		
+		int a, b, c;
+		int aa = -12, bb = 123;
+		
+		System.out.println("aa = " + aa);
+		
+		byte bbb = 0;
+		int xxx = bbb;
+		
+		bbb =(byte)xxx;
+		
+		xxx = 123456789;
+		
+		double dd = 12.5;
+		float ff = 2.45f;
+		
+		char zav = '@';
+		
+		zav = 64;
+		System.out.println("j.hrasko" + zav + "ostrovskeho.com");
+		
+		boolean prsi = true;//false
+		
+
+	}
+
+}
---
+++ MojPrvy/src/Operatory.java
@@ -0,0 +1,27 @@
+
+public class Operatory {
+
+	public static void main(String[] args) {
+		
+		//aritmeticke operatory
+		System.out.println(10 + 5);
+		System.out.println(10 - 5);
+		System.out.println(10 * 5);
+		System.out.println(10 / 5);
+		System.out.println(10 % 5);
+		
+		System.out.println("---------------");
+		//relacne operatory
+		System.out.println(10 < 5);
+		System.out.println(10 > 5);
+		System.out.println(10 <= 5);
+		System.out.println(10 >= 5);
+		System.out.println(10 == 5);
+		System.out.println(10 != 5);
+		
+		//logicke op.
+		System.out.println((10 < 5)&&!(10 <= 5)||(10 != 5));
+		
+	}
+
+}
