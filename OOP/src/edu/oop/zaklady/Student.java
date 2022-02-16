46fb490365c245455a965592d14a2142140c9401
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-02-16 09:42:45 +0100

    trieda student

--- OOP/src/edu/oop/zaklady/Student.java
+++ OOP/src/edu/oop/zaklady/Student.java
@@ -3,11 +3,52 @@ package edu.oop.zaklady;
 public class Student {
 	
 	//vlastnosti
+	//instancne premenne
 	String meno;
 	String priezvisko;
 	char pohlavie;
 	String trieda;
 	int skupina;
 	int vek;
+	
+	//schopnosti
+	//metody
+	
+	void vypisInfo() {
+		System.out.println("Meno: " + meno);
+		System.out.println("Priezvisko: " + priezvisko);
+		System.out.println("Pohlavie: " + pohlavieSlovom());
+		System.out.println("Trieda: " + trieda);
+		System.out.println("Skupina: " + skupina);
+		System.out.println("Vek: " + vek);
+	}
+	
+	String pohlavieSlovom() {
+		switch(pohlavie) {
+		case 'm':
+			return "muz";
+		case 'z':
+			return "zena";
+		case 'n':
+			return "neuvadza";
+		default:
+			return "nezname";
+		}
+	}
+	/**
+	 * Metoda vypis informacie o triede ziaka v tvare:
+	 * Meno Priezvisko: Trieda, Skupina. skupina
+	 * 
+	 */
+	void infoTrieda() {
+		
+	}
+	/**
+	 * Metoda vrati true v pripade, ze je student plnolety
+	 * @return
+	 */
+	boolean plnolety() {
+		
+	}
 
 }
--- OOP/src/edu/oop/zaklady/Tester.java
+++ OOP/src/edu/oop/zaklady/Tester.java
@@ -16,11 +16,21 @@ public class Tester {
 		s1.skupina = 1;
 		s1.trieda = "I.A";
 		s1.vek = 16;
-		System.out.println("Meno ziaka: " + s1.meno);
+		//System.out.println("Meno ziaka: " + s1.meno);
 		
+		s1.vypisInfo();
 		Student s2 = new Student();
 		
 		//nainicializovat s2
+		s2.meno = "Fero";
+		s2.priezvisko = "Mravec";
+		s2.pohlavie = 'm';
+		s2.skupina = 2;
+		s2.trieda = "I.C";
+		s2.vek = 17;
+		
+		System.out.println("--------------");
+		s2.vypisInfo();
 		//deklarovat a inicializovat s3
 		
 		
